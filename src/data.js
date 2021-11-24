e9d50d44d12dacff2698220a3ba6c4625aaffdb6
Giorgi <giorgi.bitsadze01@gmail.com>
2021-11-25 00:07:27 +0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Winter Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Popular Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4"
+    }
+]
+
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/4937223/pexels-photo-4937223.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        title: "shirt style"
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1545291730-faff8ca1d4b0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "loungewear style"
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1475180098004-ca77a66827be?ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "light style"
+    }
+]
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 2,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 3,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 4,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 5,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 6,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 7,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    },
+    {
+        id: 8,
+        img: "https://www.transparentpng.com/thumb/t-shirt/mGqRg6-black-man-t-shirt-picture.png"
+    }
+]
